8ef4cf36d2d78ef18b5b9710b9e9e60485037dbf
Rafael de sousa <sousarafael151@gmail.com>
2022-04-05 15:36:36 -0300

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+
 const { principal } = require ('../Carrinhosdecompras/src/index')
+const express = require('express')
+const path = require ('path')
+const app = express();
+const port = 3000;
+
 principal()
+
+app.get('/', express.static(path.join(__dirname, 'public/')))
+
+app.get('/', function(req, res){
+    res.send('Devolvido pelo express node')
+});
+
+//FUNÇÃO DE CALLBACK
+app.listen(port, () => {
+    console.log('Servidor funcionando ' + port)
+});
