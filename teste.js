aa612392ebcff46a23c95b89394345de2ac3139f
Rafael de sousa <sousarafael151@gmail.com>
2022-04-13 18:19:31 -0300

    criação do gitignore

---
+++ teste.js
@@ -0,0 +1,9 @@
+let destinosIncriveis = ['Marrocos', 'Bariloche', 'Barcelona', 'China', 'Grecia']
+
+let [ , item1, , item2, ] = destinosIncriveis
+
+var bariloche = item1
+
+var china = item2
+
+console.log(bariloche)
